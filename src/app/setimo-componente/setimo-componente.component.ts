178de7933f94d7d86b045d122f11413268ab8d23
italo <italo.sa01@etec.sp.gov.br>
2024-08-19 19:46:29 -0300

    angular

---
+++ src/app/setimo-componente/setimo-componente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-setimo-componente',
+  standalone: true,
+  imports: [],
+  templateUrl: './setimo-componente.component.html',
+  styleUrl: './setimo-componente.component.css'
+})
+export class SetimoComponenteComponent {
+
+}
